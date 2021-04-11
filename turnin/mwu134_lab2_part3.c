9eda5833fc8726450533fac4f7eb14c7361dc9b2
Min Chen Wu <mwu134@ucr.edu>
2021-04-10 21:10:29 -0700

    Completed part 3

--- turnin/mwu134_lab2_part3.c
+++ turnin/mwu134_lab2_part3.c
@@ -1,11 +1,11 @@
 /*	Author: mwu134
- *  Partner(s) Name: Min Chen Wu
- *	Lab Section: 22
- *	Assignment: Lab 2  Exercise 3
- *	Exercise Description: [optional - include for your own benefit]
- *
- *	I acknowledge all content contained herein, excluding template or example
- *	code, is my own original work.
+ *	Partner(s) Name: Min Chen Wu
+ *      Lab Section: 22
+ *      Assignment: Lab 2  Exercise 2
+ *      Exercise Description: [optional - include for your own benefit]
+ *      
+ *      I acknowledge all content contained herein, excluding template or example
+ *      code, is my own original work.
  */
 #include <avr/io.h>
 #ifdef _SIMULATE_
@@ -14,32 +14,31 @@
 
 int main(void) {
     /* Insert DDR and PORT initializations */
-	DDRB = 0xFF; PORTB = 0x00;
-	DDRA = 0x00; PORTA = 0xFF;
+        DDRB = 0xFF; PORTB = 0x00;
+        DDRA = 0x00; PORTA = 0xFF;
     /* Insert your solution below */
 
     unsigned char tmpA = 0x00;
-    //unsigned char tmpB = 0x00;
+    unsigned char tmpB = 0x00;
     unsigned char cntavail = 0x00;
     while (1) {
-	//input
-	tmpA = PINA & 0xFF;
-	//computation
-	cntavail = 0x00;
-	int i;
-	if(tmpA == 0xFF) PORTC = 0x80;
-	else {
-	for( i = 0; i < 7; i = i + 1){
-		if((tmpA & 0x01) == 0x01){
-			cntavail = cntavail + 1;
-		}
-		tmpA = tmpA >> 1;
-	}
-	//tmpA = tmpA >> 1;
-	//output
-	PORTC = cntavail;
-	}
-    }
-    return 0;
-
-}
+        //input
+        tmpA = PINA & 0x0F;
+        //computation
+        cntavail = 0x00;
+        int i;
+	if((tmpA & 0x0F) == 0x0F){
+		PORTC = 0x80;
+	}else{
+        	for( i = 0; i < 4; i = i + 1){
+        		if((tmpA & 0x01) == 0x00){
+        			cntavail = cntavail + 1; 
+                	}
+                	tmpA = tmpA >> 1;                                                 
+       		}
+        //output                                               
+ 	       PORTC = cntavail;          
+        	}
+	}	
+        return 0;                                 
+}                                                                                                                                                  
--- turnin/mwu134_lab2_part3_tests.py
+++ turnin/mwu134_lab2_part3_tests.py
@@ -1,61 +1,44 @@
-# Array of tests to run (in order)
-# Each test contains
-#   description - 
-#   steps - A list of steps to perform, each step can have
-#       inputs - A list of tuples for the inputs to apply at that step
-#       *time - The time (in ms) to wait before continuing to the next step 
-#           and before checking expected values for this step. The time should be a multiple of
-#           the period of the system
-#       *iterations - The number of clock ticks to wait (periods)
-#       expected - The expected value at the end of this step (after the "time" has elapsed.) 
-#           If this value is incorrect the test will fail early before completing.
-#       * only one of these should be used
-#   expected - The expected output (as a list of tuples) at the end of this test
-# An example set of tests is shown below. It is important to note that these tests are not "unit tests" in 
-# that they are not ran in isolation but in the order shown and the state of the device is not reset or 
-# altered in between executions (unless preconditions are used
+/*	Author: mwu134
+ *	Partner(s) Name: Min Chen Wu
+ *      Lab Section: 22
+ *      Assignment: Lab 2  Exercise 2
+ *      Exercise Description: [optional - include for your own benefit]
+ *      
+ *      I acknowledge all content contained herein, excluding template or example
+ *      code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
 
-tests = [ {'description': 'PINA: 0x00 => PORTC: 0x00',
-    'steps': [ {'inputs': [('PINA',0x00)], 'iterations': 5 } ],
-    'expected': [('PORTC',0x00)],
-    },
-    {'description': 'PINA: 0x01 => PORTC: 0x01',
-    'steps': [ {'inputs': [('PINA',0x01)], 'iterations': 5 } ],
-    'expected': [('PORTC',0x01)],
-    },
-    {'description': 'PINA: 0x02 => PORTC: 0x01',
-    'steps': [ {'inputs': [('PINA',0x02)], 'iterations': 5 } ],
-    'expected': [('PORTC',0x01)],
-    },
-    {'description': 'PINA: 0x03 => PORTC: 0x02',
-    'steps': [ {'inputs': [('PINA',0x03)], 'iterations': 5 } ],
-    'expected': [('PORTC',0x02)],
-    },
-    {'description': 'PINA: 0x08 => PORTC: 0x01',
-    'steps': [ {'inputs': [('PINA',0x8)], 'iterations': 5 } ],
-    'expected': [('PORTC',0x01)],
-    },
-    {'description': 'PINA: 0x0A => PORTC: 0x02',
-    'steps': [ {'inputs': [('PINA',0xA)], 'iterations': 5 } ],
-    'expected': [('PORTC',0x02)],
-    },
-    {'description': 'PINA: 0xFF => PORTC: 0x80',
-    'steps': [ {'inputs': [('PINA',0xFF)], 'iterations': 5 } ],
-    'expected': [('PORTC',0x80)],
-    },
-    {'description': 'PINA: 0x2A => PORTC: 0x03',
-    'steps': [ {'inputs': [('PINA',0x2A)], 'iterations': 5 } ],
-    'expected': [('PORTC',0x03)],
-    },
+int main(void) {
+    /* Insert DDR and PORT initializations */
+        DDRB = 0xFF; PORTB = 0x00;
+        DDRA = 0x00; PORTA = 0xFF;
+    /* Insert your solution below */
 
-
-    ]
-watch = ['cntavail']
-
-
-# Optionally you can add a set of "watch" variables these need to be global or static and may need
-# to be scoped at the function level (for static variables) if there are naming conflicts. The 
-# variables listed here will display everytime you hit (and stop at) a breakpoint
-#watch = ['<function>::<static-var>','PORTB']
-
-
+    unsigned char tmpA = 0x00;
+    unsigned char tmpB = 0x00;
+    unsigned char cntavail = 0x00;
+    while (1) {
+        //input
+        tmpA = PINA & 0x0F;
+        //computation
+        cntavail = 0x00;
+        int i;
+	if((tmpA & 0x0F) == 0x0F){
+		PORTC = 0x80;
+	}else{
+        	for( i = 0; i < 4; i = i + 1){
+        		if((tmpA & 0x01) == 0x00){
+        			cntavail = cntavail + 1; 
+                	}
+                	tmpA = tmpA >> 1;                                                 
+       		}
+        //output                                               
+ 	       PORTC = cntavail;          
+        	}
+	}	
+        return 0;                                 
+}                                                                                                                                                  
